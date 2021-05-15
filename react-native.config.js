5d66a32aeb6e36f23f2d841d34b9fc5912f36f60
Leonardo Soares <leonardoxp9d@gmail.com>
2021-05-15 09:21:52 -0300

    Initial commit

---
+++ react-native.config.js
@@ -0,0 +1,11 @@
+/** 3.3- Iniciando aplicativo mobile - 1.Estrutura de padrões - 5.Importando fontes externas */
+
+module.exports = {
+    project: {
+        ios: {},
+        android: {},
+    },
+    assets: [
+        './assets/fonts/'
+    ],
+};
